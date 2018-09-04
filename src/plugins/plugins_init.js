3eba3ad547cd91dec56b2cc106624356b02292d5
JiHong.Lee <dlwlghd88@gmail.com>
2018-09-04 15:11:14 +0900

    added: plugins_init.js

---
+++ src/plugins/plugins_init.js
@@ -0,0 +1,17 @@
+import align from './submenu/align'
+import font from './submenu/font'
+import fontSize from './submenu/fontSize'
+import fontColor from './submenu/fontColor'
+import hiliteColor from './submenu/hiliteColor'
+import horizontalRule from './submenu/horizontalRule'
+import list from './submenu/list'
+import table from './submenu/table'
+import formatBlock from './submenu/formatBlock'
+
+import link from './dialog/link'
+import image from './dialog/image'
+import video from './dialog/video'
+
+import module_dialog from './modules/dialog'
+
+export {align, font, fontSize, fontColor, hiliteColor, horizontalRule, list, table, formatBlock, link, image, video, module_dialog}
