2f31ff8d0df6577a875189e52bbfae92c40cd3aa
Luan Peil <luan@peil.dev>
2019-12-08 22:51:15 -0300

    Add Brazilian Portuguese

--- src/lang/index.js
+++ src/lang/index.js
@@ -7,7 +7,8 @@ import fr from './fr';
 import ja from './ja';
 import ko from './ko';
 import ru from './ru';
+import pt_br from './pt_br';
 import zh_cn from './zh_cn';
 
-export { da, de, en, fr, ja, ko, ru, zh_cn };
-export default { da, de, en, fr, ja, ko, ru, zh_cn };
+export { da, de, en, fr, ja, ko, ru, zh_cn, pt_br };
+export default { da, de, en, fr, ja, ko, ru, zh_cn, pt_br };
