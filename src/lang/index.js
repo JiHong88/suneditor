e8c99d09ce750f657fda0e622c60c22c38e444b2
Luan Peil <luan@peil.dev>
2019-12-08 23:11:39 -0300

    Normalize

--- src/lang/index.js
+++ src/lang/index.js
@@ -6,9 +6,9 @@ import en from './en';
 import fr from './fr';
 import ja from './ja';
 import ko from './ko';
-import ru from './ru';
 import pt_br from './pt_br';
+import ru from './ru';
 import zh_cn from './zh_cn';
 
-export { da, de, en, fr, ja, ko, ru, zh_cn, pt_br };
-export default { da, de, en, fr, ja, ko, ru, zh_cn, pt_br };
+export { da, de, en, fr, ja, ko, pt_br, ru, zh_cn };
+export default { da, de, en, fr, ja, ko, pt_br, ru,zh_cn };
