d8d09ae0c3434c6534932cbc116c301d39491455
Juan Carlos Pinzón <juancarlospinzon@outlook.com>
2019-12-12 10:10:46 -0500

    Update index.js
    Add es.js lang to index.js lang

--- src/lang/index.js
+++ src/lang/index.js
@@ -9,6 +9,7 @@ import ko from './ko';
 import pt_br from './pt_br';
 import ru from './ru';
 import zh_cn from './zh_cn';
+import es from './es'
 
-export { da, de, en, fr, ja, ko, pt_br, ru, zh_cn };
-export default { da, de, en, fr, ja, ko, pt_br, ru,zh_cn };
+export { da, de, en, fr, ja, ko, pt_br, ru, zh_cn, es };
+export default { da, de, en, fr, ja, ko, pt_br, ru, zh_cn, es };
