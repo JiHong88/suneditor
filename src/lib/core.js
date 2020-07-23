bc8a7b8fa072347e66f1a8f95d67119f0ab030a6
JiHong88 <0125ses@hanmail.net>
2020-07-23 14:17:08 +0900

    fix: #446 onload event

--- src/lib/core.js
+++ src/lib/core.js
@@ -4916,7 +4916,9 @@ export default function (context, pluginCallButtons, plugins, lang, options, _re
             this.history.reset(true);
             this._resourcesStateChange();
 
-            if (typeof functions.onload === 'function') return functions.onload(this, reload);
+            _w.setTimeout(function () {
+              if (typeof functions.onload === 'function') functions.onload(core, reload);
+            });
         },
 
         /**
