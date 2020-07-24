d35b3c84269dbb8fbce6ff0cd8abcf334a7f3d45
Kostya Mikhailyuk <skivsoft@gmail.com>
2020-07-25 00:16:53 +0300

    Latvian translation

--- src/lang/index.js
+++ src/lang/index.js
@@ -8,11 +8,12 @@ import fr from './fr';
 import it from './it';
 import ja from './ja';
 import ko from './ko';
+import lv from './lv';
 import pl from './pl';
 import pt_br from './pt_br';
 import ro from './ro';
 import ru from './ru';
 import zh_cn from './zh_cn';
 
-export { da, de, en, es, fr, it, ja, ko, pl, pt_br, ro, ru, zh_cn };
-export default { da, de, en, es, fr, it, ja, ko, pl, pt_br, ro, ru, zh_cn };
+export { da, de, en, es, fr, it, ja, ko, lv, pl, pt_br, ro, ru, zh_cn };
+export default { da, de, en, es, fr, it, ja, ko, lv, pl, pt_br, ro, ru, zh_cn };
