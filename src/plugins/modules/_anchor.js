cacc079e10f69a8153d0c2ddd783337e268db8e4
JiHong88 <0125ses@hanmail.net>
2021-02-21 04:32:57 +0900

    fix: link path

--- src/plugins/modules/_anchor.js
+++ src/plugins/modules/_anchor.js
@@ -190,7 +190,7 @@ export default {
         const value = typeof e === 'string' ? e : e.target.value.trim();
         const reservedProtocol  = /^(mailto\:|https*\:\/\/)/.test(value);
         const sameProtocol = !protocol ? false : this._w.RegExp('^' + value.substr(0, protocol.length)).test(protocol);
-        context.linkValue = preview.textContent = !value ? '' : (protocol && !reservedProtocol && !sameProtocol) ? protocol + value : reservedProtocol ? value : /^www\./.test(value) ? 'http://' + value : this.context.anchor.host + value;
+        context.linkValue = preview.textContent = !value ? '' : (protocol && !reservedProtocol && !sameProtocol) ? protocol + value : reservedProtocol ? value : /^www\./.test(value) ? 'http://' + value : this.context.anchor.host + (/^\//.test(value) ? '' : '/') + value;
 
         if (value.indexOf('#') === 0) {
             context.bookmark.style.display = 'block';
