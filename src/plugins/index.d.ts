16dbb16415966c0f3c33f54e0cf3f857d43760c0
JiHong88 <0125ses@hanmail.net>
2021-04-27 22:12:14 +0900

    fix: #727  typescript build

--- src/plugins/index.d.ts
+++ src/plugins/index.d.ts
@@ -1,32 +1,79 @@
 // command
-import blockquote from './command/blockquote';
+import { CommandPlugin } from "./CommandPlugin";
+import blockquote from "./command/blockquote";
 
 // submenu
-import align from './submenu/align';
-import font from './submenu/font';
-import fontSize from './submenu/fontSize';
-import fontColor from './submenu/fontColor';
-import hiliteColor from './submenu/hiliteColor';
-import horizontalRule from './submenu/horizontalRule';
-import list from './submenu/list';
-import table from './submenu/table';
-import formatBlock from './submenu/formatBlock';
-import lineHeight from './submenu/lineHeight';
-import template from './submenu/template';
-import paragraphStyle from './submenu/paragraphStyle';
-import textStyle from './submenu/textStyle';
+import { SubmenuPlugin } from "./SubmenuPlugin";
+import align from "./submenu/align";
+import font from "./submenu/font";
+import fontSize from "./submenu/fontSize";
+import fontColor from "./submenu/fontColor";
+import hiliteColor from "./submenu/hiliteColor";
+import horizontalRule from "./submenu/horizontalRule";
+import list from "./submenu/list";
+import table from "./submenu/table";
+import formatBlock from "./submenu/formatBlock";
+import lineHeight from "./submenu/lineHeight";
+import template from "./submenu/template";
+import paragraphStyle from "./submenu/paragraphStyle";
+import textStyle from "./submenu/textStyle";
 
 // dialog
-import link from './dialog/link';
-import image from './dialog/image';
-import video from './dialog/video';
-import audio from './dialog/audio';
-import math from './dialog/math';
+import { DialogPlugin } from "./DialogPlugin";
+import link from "./dialog/link";
+import image from "./dialog/image";
+import video from "./dialog/video";
+import audio from "./dialog/audio";
+import math from "./dialog/math";
 
 // file browser
-import imageGallery from './fileBrowser/imageGallery';
+import { FileBrowserPlugin } from "./FileBrowserPlugin";
+import imageGallery from "./fileBrowser/imageGallery";
 
-declare const _default: { blockquote, align, font, fontSize, fontColor, hiliteColor, horizontalRule, list, table, formatBlock, lineHeight, template, paragraphStyle, textStyle, link, image, video, audio, math, imageGallery };
+declare const _default: {
+  blockquote: CommandPlugin;
+  align: SubmenuPlugin;
+  font: SubmenuPlugin;
+  fontSize: SubmenuPlugin;
+  fontColor: SubmenuPlugin;
+  hiliteColor: SubmenuPlugin;
+  horizontalRule: SubmenuPlugin;
+  list: SubmenuPlugin;
+  table: SubmenuPlugin;
+  formatBlock: SubmenuPlugin;
+  lineHeight: SubmenuPlugin;
+  template: SubmenuPlugin;
+  paragraphStyle: SubmenuPlugin;
+  textStyle: SubmenuPlugin;
+  link: DialogPlugin;
+  image: DialogPlugin;
+  video: DialogPlugin;
+  audio: DialogPlugin;
+  math: DialogPlugin;
+  imageGallery: FileBrowserPlugin;
+};
 
-export { blockquote, align, font, fontSize, fontColor, hiliteColor, horizontalRule, list, table, formatBlock, lineHeight, template, paragraphStyle, textStyle, link, image, video, audio, math, imageGallery };
+export {
+  blockquote,
+  align,
+  font,
+  fontSize,
+  fontColor,
+  hiliteColor,
+  horizontalRule,
+  list,
+  table,
+  formatBlock,
+  lineHeight,
+  template,
+  paragraphStyle,
+  textStyle,
+  link,
+  image,
+  video,
+  audio,
+  math,
+  imageGallery,
+};
+
 export default _default;
