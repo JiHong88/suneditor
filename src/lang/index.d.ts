b499ef71d71e1c69c00e5aabf83808b7ad4fdaba
JiHong88 <0125ses@hanmail.net>
2021-06-11 17:49:26 +0900

    fix: #759 nl translation

--- src/lang/index.d.ts
+++ src/lang/index.d.ts
@@ -9,6 +9,7 @@ import it from './it';
 import ja from './ja';
 import ko from './ko';
 import lv from './lv';
+import nl from './nl';
 import pl from './pl';
 import pt_br from './pt_br';
 import ro from './ro';
@@ -17,4 +18,4 @@ import se from './se';
 import ua from './ua';
 import zh_cn from './zh_cn';
 
-export { ckb, da, de, en, es, fr, he, it, ja, ko, lv, pl, pt_br, ro, ru, se, ua, zh_cn };
+export { ckb, da, de, en, es, fr, he, it, ja, ko, lv, nl, pl, pt_br, ro, ru, se, ua, zh_cn };
