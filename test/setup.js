7224be97f034bf63c60b78436bf2f28a72cd1639
jihong88 <0125ses@hanmail.net>
2025-09-19 00:20:40 +0900

    test: unit update

--- test/setup.js
+++ test/setup.js
@@ -12,18 +12,39 @@ global.ResizeObserver = class ResizeObserver {
 };
 
 // Mock XMLHttpRequest if needed
-global.XMLHttpRequest = global.XMLHttpRequest || class XMLHttpRequest {
-	constructor() {
-		this.readyState = 0;
-		this.status = 0;
-		this.statusText = '';
-		this.responseText = '';
-		this.response = '';
-	}
-	open() {}
-	send() {}
-	setRequestHeader() {}
-	abort() {}
-};
+global.XMLHttpRequest =
+	global.XMLHttpRequest ||
+	class XMLHttpRequest {
+		constructor() {
+			this.readyState = 0;
+			this.status = 0;
+			this.statusText = '';
+			this.responseText = '';
+			this.response = '';
+		}
+		open() {}
+		send() {}
+		setRequestHeader() {}
+		abort() {}
+	};
 
-// Add any other global mocks or setup here
+// Mock visualViewport
+if (!('visualViewport' in window)) {
+	Object.defineProperty(window, 'visualViewport', {
+		configurable: true,
+		value: {
+			width: 1200,
+			height: 800,
+			scale: 1,
+			offsetTop: 0,
+			offsetLeft: 0,
+			addEventListener: jest.fn(),
+			removeEventListener: jest.fn(),
+			_listeners: {},
+			dispatchEvent(ev) {
+				const list = this._listeners?.[ev.type] || [];
+				list.forEach((fn) => fn(ev));
+			}
+		}
+	});
+}
